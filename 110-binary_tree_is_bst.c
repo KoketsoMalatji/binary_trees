10dade9b4b95550d2d36715b4751e433b4a1d66d
KoketsoMalatji <koketsosean209@gmail.com>
2022-08-26 06:20:28 -0700

    Is bst

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,48 +1,89 @@
 #include "binary_trees.h"
 
-int recursive_bst_check(const binary_tree_t *tree, int min, int max);
-
 /**
- * binary_tree_is_bst - Checks if binary tree is a valid BST
+ * binary_tree_is_leaf - checks if a node is a leaf
  *
- * @tree:  root node of the tree to rotate
- *
- * Return: 1 if BST, 0 if not
+ * @node: node to check
+ * Return: 1 if node is a leaf, otherwise 0
  */
-
-int binary_tree_is_bst(const binary_tree_t *tree)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	return (recursive_bst_check(tree, INT_MIN, INT_MAX));
+	int leaf = 0;
+
+	if (node && !(node->left) && !(node->right))
+		leaf = 1;
+
+	return (leaf);
 }
 
+/**
+ * check_parent - checks if node has a lower/higher than its grand parent
+ *
+ * @tree: tree node
+ * Return: 1 if actual node value, 0 otherwise
+ */
+int check_parent(const binary_tree_t *tree)
+{
+	const binary_tree_t *prnt;
+	const binary_tree_t *grand_prnt;
+
+	if (tree == NULL || tree->parent == NULL || tree->parent->parent == NULL)
+		return (1);
+
+	prnt = tree->parent;
+	grand_prnt = prnt->parent;
+
+	while (prnt && grand_prnt)
+	{
+		if (prnt->n < grand_prnt->n && tree->n >= grand_prnt->n)
+			return (0);
+
+		if (prnt->n > grand_prnt->n && tree->n <= grand_prnt->n)
+			return (0);
+
+		prnt = prnt->parent;
+		grand_prnt = prnt->parent;
+	}
+
+	return (1);
+}
 
 /**
- * recursive_bst_check - Checks if binary tree is a valid BST
+ * check_is_bst - checks if binary tree is a BST
  *
- * @tree:  root node of the tree to rotate
- *
- * @min: Min value
- *
- * @max: Max value
- *
- * Return: 1 if BST, 0 if not
+ * @tree: tree root
+ * Return: 1 if tree is a BST, 0 otherwise
  */
-
-int recursive_bst_check(const binary_tree_t *tree, int min, int max)
+int check_is_bst(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
+	if (!tree)
 		return (1);
-	}
 
-	if (tree->n < min || tree->n > max)
-	{
+	if (binary_tree_is_leaf(tree))
+		return (1);
+
+	if (tree->left && tree->left->n >= tree->n)
 		return (0);
-	}
-	return(recursive_bst_check(tree->left, min, tree->n - 1)
-	       && recursive_bst_check(tree->right, tree->n + 1, max));
+
+	if (tree->right && tree->right->n <= tree->n)
+		return (0);
+
+	if (!check_parent(tree->left) || !check_parent(tree->right))
+		return (0);
+
+	return (check_is_bst(tree->left) && check_is_bst(tree->right));
+}
+
+/**
+ * binary_tree_is_bst - calls to check_is_bst to check if tree is BST
+ *
+ * @tree: tree root
+ * Return: 1 if tree is a BST, 0 otherwise
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (check_is_bst(tree));
 }
